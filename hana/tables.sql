64fe51fc8146ae72e7f488223dfa7cd04be41a4e
Ingo Classen <ingo.classen@htw-berlin.de>
2023-03-29 11:39:48 +0200

    wip

--- hana/tables.sql
+++ hana/tables.sql
@@ -2,11 +2,6 @@ drop table order_products__prior;
 drop table orders;
 drop table submission;
 
-create column table aisles (
-  aisle_id integer not null primary key,
-  aisle varchar(100)
-);
-
 create column table orders (
   order_id integer not null primary key,
   user_id varchar(100) not null,
@@ -29,7 +24,16 @@ create column table submission (
 );
 
 
+
+
 alter table order_products__prior
     add constraint fk1 foreign key (order_id) references orders (order_id);
 
 create btree index i1 on order_products__prior (order_id);
+
+
+create column table aisles (
+  aisle_id integer not null primary key,
+  aisle varchar(100)
+);
+
